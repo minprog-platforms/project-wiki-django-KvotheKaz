fea20879dcb8a8126c105bd5fa709ebeff8d15b9
Nikki van der Woord <nikkivanderwoord@gmail.com>
2021-12-06 22:55:15 +0100

    Functional wiki page

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -3,5 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("search", views.search, name="search"),
+    path("new", views.new_entry, name="new"),
+    path("", views.index, name="index"),
+    path("wiki/<str:title>", views.entry, name="entry"),
+    path("random", views.random_page, name="random")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,10 +1,61 @@
-from django.shortcuts import render
-
+import markdown2
+from django.shortcuts import redirect, render
+from django import forms
+import random
+from django.db import models
 from . import util
 
 
+
+class NewEntryForm(forms.Form):
+    new_title = forms.CharField(label="Title")
+    new_content = forms.CharField(label="Content")
+
 def index(request):
-    return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
+    return render (request, "encyclopedia/index.html", {
+        "entries": util.list_entries(),
+    })
+
+def entry(request, title):
+    return render (request, "encyclopedia/entry.html", {
+        "title": title,
+        "content": markdown2.markdown(util.get_entry(title))
+    })
+
+def random_page(request):
+    pages = util.list_entries()
+    random_page = random.choice(pages)
+    return entry(request, random_page)
+
+def new_entry(request):
+    if request.method == "POST":
+        form = NewEntryForm(request.POST)
+        if form.is_valid():
+            title = form.cleaned_data["new_title"]
+            content = form.cleaned_data["new_content"]
+            new_entry = util.save_entry(title, content)
+            return render(request, "encyclopedia/new.html", {
+                "new_entry": new_entry,
+            })
+        else:
+            return render(request, "encyclopedia/new.html", {
+                "form": form,
+            })
+    return render(request, "encyclopedia/new.html", {
+        "form": NewEntryForm()
+    })
+
+def search(request):
+    entries = util.list_entries()
+    query = request.GET.get("q", "")
+    if util.get_entry(query):
+        return redirect('entry', title = query)
+    else:
+        substring_entries = []
+        for entry in entries:
+            if query.upper() in entry.upper():
+                substring_entries.append(entry)
+        return render(request, "encyclopedia/search.html", {
+            "entries": substring_entries,
     })
 
